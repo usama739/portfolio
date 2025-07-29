c9f9c2935df33cb82bd5c78c6fd11d70e81292c9
Usama Aslam <usamaaslam739@gmail.com>
2025-07-29 23:36:16 +0500

    fixing issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				<link rel="icon" href="/assets/web-development.png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
